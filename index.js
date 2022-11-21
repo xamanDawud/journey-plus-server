7b012badb38cee65604436f705ddc4fd8b674e12
xamanDawud <ahmeddaudking100@gmail.com>
2022-11-21 14:32:25 +0600

    romes added and vercel deploy dong

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("express");
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+app.use(cors());
+
+let romes = require("./data/romes.json");
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+app.get("/romes", (req, res) => {
+  res.send(romes);
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Port is running on port ${port}`);
+});
